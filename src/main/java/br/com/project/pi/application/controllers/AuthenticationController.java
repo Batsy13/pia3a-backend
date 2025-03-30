4b5c6b1d7fdf248fc7ccb0d43a4fbae41e7a04ee
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-03-30 12:08:44 -0300

    feat: add tokenization to the registry endpoiint

--- src/main/java/br/com/project/pi/application/controllers/AuthenticationController.java
+++ src/main/java/br/com/project/pi/application/controllers/AuthenticationController.java
@@ -3,7 +3,6 @@ package br.com.project.pi.application.controllers;
 import br.com.project.pi.application.dto.AuthenticationDTO;
 import br.com.project.pi.application.dto.EmailResponseDTO;
 import br.com.project.pi.application.dto.RegisterDTO;
-import br.com.project.pi.application.dto.UserDTO;
 import br.com.project.pi.application.infra.segurity.TokenService;
 import br.com.project.pi.application.model.User;
 import br.com.project.pi.application.repositories.UserRepository;
@@ -19,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/api/auth")
+
 public class AuthenticationController {
 
     @Autowired
@@ -32,7 +32,8 @@ public class AuthenticationController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+
         var userNamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = this.authenticationManager.authenticate(userNamePassword);
 
@@ -42,14 +43,19 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
-        if(this.repository.findByEmail(data.email()).isPresent()) return ResponseEntity.badRequest().build();
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+        if (this.repository.findByEmail(data.email()).isPresent()) return ResponseEntity.badRequest().build();
 
-        String encryptedPassord = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.email(), encryptedPassord);
+        String encryptedPassord = new BCryptPasswordEncoder().encode(data.password()); /// GUARDA UMA SENHA CRIPTOGRAFADA
 
-        this.repository.save(newUser);
+        User newUser = new User(data.name(), data.email(), encryptedPassord);
+        this.repository.save(newUser); /// CRIA E SALVA UM USUARIO NO BANCO DE DADOS
 
-        return ResponseEntity.ok(new UserDTO(newUser));
+        var userNamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(userNamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new EmailResponseDTO(token));
     }
 }
