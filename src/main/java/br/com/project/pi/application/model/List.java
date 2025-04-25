7c918ce378c81300ee9da40ca28daf19debfebda
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-25 20:30:30 -0300

    feature: add class Place with pre configurations

---
+++ src/main/java/br/com/project/pi/application/model/List.java
@@ -0,0 +1,29 @@
+package br.com.project.pi.application.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_list")
+public class List {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String icon;
+
+    @OneToMany(mappedBy = "list", orphanRemoval = true, cascade = CascadeType.ALL)
+    private java.util.List<Place> places = new ArrayList<>();
+
+}
