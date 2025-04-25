e0c055e2cf69ca12436e00cd9a76e7c1c91b2de7
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-25 20:29:22 -0300

    feature: add class List with pre configurations

---
+++ src/main/java/br/com/project/pi/application/model/Place.java
@@ -0,0 +1,32 @@
+package br.com.project.pi.application.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_place")
+@Entity
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String description;
+    private byte[] image;
+    private LocalDateTime addedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private List list;
+
+}
