ca9ec06027ef978001a049fd1096bd99c3bb73ca
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-25 21:11:11 -0300

    refactor: name change

--- src/main/java/br/com/project/pi/application/repositories/ListsRepository.java
+++ src/main/java/br/com/project/pi/application/repositories/ListsRepository.java
@@ -3,5 +3,5 @@ package br.com.project.pi.application.repositories;
 import br.com.project.pi.application.model.Lists;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListRepository extends JpaRepository<Lists, Long> {
+public interface ListsRepository extends JpaRepository<Lists, Long> {
 }
