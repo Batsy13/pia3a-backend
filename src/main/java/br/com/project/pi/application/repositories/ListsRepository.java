eeb8e5677feca4b3df1c2904130793df42182cc5
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-27 13:16:10 -0300

    feature: add method findAll()

--- src/main/java/br/com/project/pi/application/repositories/ListsRepository.java
+++ src/main/java/br/com/project/pi/application/repositories/ListsRepository.java
@@ -3,5 +3,9 @@ package br.com.project.pi.application.repositories;
 import br.com.project.pi.application.model.Lists;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ListsRepository extends JpaRepository<Lists, Long> {
+
+    List<Lists> findAll();
 }
